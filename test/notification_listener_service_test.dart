dd466586f7c4ca88b935bc96a323d804e3e4ecd8
X-SLAYER <slayertn1999@gmail.com>
2023-11-18 10:23:39 +0100

    update gradle + add unit tests

--- test/notification_listener_service_test.dart
+++ test/notification_listener_service_test.dart
@@ -1,18 +1,71 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:notification_listener_service/notification_listener_service.dart';
 
 void main() {
-  const MethodChannel channel = MethodChannel('notification_listener_service');
-
   TestWidgetsFlutterBinding.ensureInitialized();
+  const MethodChannel methodChannel =
+      MethodChannel('x-slayer/notifications_channel');
 
   setUp(() {
-    channel.setMockMethodCallHandler((MethodCall methodCall) async {
-      return '42';
+    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+        .setMockMethodCallHandler(methodChannel, (MethodCall methodCall) async {
+      switch (methodCall.method) {
+        case 'requestPermission':
+          return true;
+        case 'isPermissionGranted':
+          return true;
+        default:
+          return null;
+      }
     });
   });
 
   tearDown(() {
-    channel.setMockMethodCallHandler(null);
+    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+        .setMockMethodCallHandler(methodChannel, null);
+  });
+
+  group('NotificationListenerService', () {
+    test('requestPermission returns true when permission is granted', () async {
+      final result = await NotificationListenerService.requestPermission();
+      expect(result, isTrue);
+    });
+
+    test('requestPermission returns false and logs on PlatformException',
+        () async {
+      TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+          .setMockMethodCallHandler(methodChannel,
+              (MethodCall methodCall) async {
+        if (methodCall.method == 'requestPermission') {
+          throw PlatformException(code: 'PERMISSION_DENIED');
+        }
+        return null;
+      });
+
+      final result = await NotificationListenerService.requestPermission();
+      expect(result, isFalse);
+    });
+
+    test('isPermissionGranted returns true when permission is granted',
+        () async {
+      final result = await NotificationListenerService.isPermissionGranted();
+      expect(result, isTrue);
+    });
+
+    test('isPermissionGranted returns false and logs on PlatformException',
+        () async {
+      TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+          .setMockMethodCallHandler(methodChannel,
+              (MethodCall methodCall) async {
+        if (methodCall.method == 'isPermissionGranted') {
+          throw PlatformException(code: 'PERMISSION_DENIED');
+        }
+        return null;
+      });
+
+      final result = await NotificationListenerService.isPermissionGranted();
+      expect(result, isFalse);
+    });
   });
 }
